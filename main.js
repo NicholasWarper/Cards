3f91f432f4c184d810b55a6e32ef561a58ce4c79
NicholasWarper <abesadzenika6@gmail.com>
2025-03-23 15:01:51 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
+const holder = document.querySelector(".holder-name");
+const holderInput = document.querySelector(".holder-input");
 
+console.log(holder);
+console.log(holderInput);
+
+holderInput.addEventListener("input", () => {
+    holder.textContent = holderInput.value;
+});
+
+const cred = document.querySelector(".credit");
+const credin = document.querySelector(".credit-number");
+
+console.log(cred);
+console.log(credin);
+
+cred.addEventListener("input", () => {
+    credin.textContent = cred.value;
+});
